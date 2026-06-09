10634d32cc5861e605545679f8ef65198891e592
matricarin <matricarin@gmail.com>
2026-06-09 14:43:12 +0500

    interfaces was added

---
+++ src/SmartMealService.Services/IMenuRepository.cs
@@ -0,0 +1,8 @@
+﻿using SmartMealService.Domain.Models;
+
+namespace SmartMealService.Infrastructure;
+
+public interface IMenuRepository
+{
+    Task SendMenuItems(List<MenuItem>  menuItems);
+}
---
+++ src/SmartMealService.Services/IOrderingService.cs
@@ -0,0 +1,9 @@
+﻿using SmartMealService.Services.Dtos;
+
+namespace SmartMealService.Services;
+
+public interface IOrderingService
+{
+    Task<List<MenuItemDto>> GetMenu(RequestDto requestDto);
+    Task<ResponseDto> SendOrder(RequestDto requestDto);
+}
