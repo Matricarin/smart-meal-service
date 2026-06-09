f951b8d6a0645bb79e25bdd77f9ef3279b9c4482
matricarin <matricarin@gmail.com>
2026-06-09 15:06:03 +0500

    logging and interfaces were added

--- src/SmartMealService.Infrastructure/MenuRepository.cs
+++ src/SmartMealService.Infrastructure/MenuRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace SmartMealService.Infrastructure;
+﻿using Serilog;
 
-public sealed class MenuRepository
+using SmartMealService.Domain.Models;
+
+namespace SmartMealService.Infrastructure;
+
+public sealed class MenuRepository : IMenuRepository
 {
-    
+    private readonly ILogger _logger;
+
+    public Task SaveMenuItems(List<MenuItem> menuItems)
+    {
+        throw new NotImplementedException();
+    }
 }
