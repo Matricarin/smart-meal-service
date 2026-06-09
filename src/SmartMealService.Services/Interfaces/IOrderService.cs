ff41a2c93a36bca944c7e197321e1e51cd4bf2ba
matricarin <matricarin@gmail.com>
2026-06-09 15:50:20 +0500

    contracts were changed

---
+++ src/SmartMealService.Services/Interfaces/IOrderService.cs
@@ -0,0 +1,9 @@
+﻿using SmartMealService.Domain.Models;
+
+namespace SmartMealService.Services.Interfaces;
+
+public interface IOrderService
+{
+    Task<List<MenuItem>> GetMenuAsync(bool withPrice, CancellationToken cancellationToken);
+    Task<bool> SendOrderAsync(Order order, CancellationToken cancellationToken);
+}
