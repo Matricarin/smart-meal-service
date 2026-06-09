a60bfe5ea9f8752386721dfb12e1a31bc565fca1
matricarin <matricarin@gmail.com>
2026-06-09 14:42:43 +0500

    business logic models draft

---
+++ src/SmartMealService.Domain/Models/Order.cs
@@ -0,0 +1,7 @@
+﻿namespace SmartMealService.Domain.Models;
+
+public sealed class Order
+{
+    public required Guid Id { get; init; }
+    public required List<OrderingItem> MenuItems { get; init; }
+}
---
+++ src/SmartMealService.Domain/Models/OrderingItem.cs
@@ -0,0 +1,7 @@
+﻿namespace SmartMealService.Domain.Models;
+
+public sealed class OrderingItem
+{
+    public required MenuItem MenuItem { get; init; }
+    public required float Quantity { get; init; }
+}
