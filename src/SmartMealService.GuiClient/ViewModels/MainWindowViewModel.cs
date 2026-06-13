f0dc63f438462699adfc0709efb35a52d0a453e8
matricarin <matricarin@gmail.com>
2026-06-13 13:18:00 +0500

    view models were updated

--- src/SmartMealService.GuiClient/ViewModels/MainWindowViewModel.cs
+++ src/SmartMealService.GuiClient/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,64 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.ObjectModel;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+using Serilog;
+
+using SmartMealService.GuiClient.Application;
 
 namespace SmartMealService.GuiClient.ViewModels;
 
 public sealed partial class MainWindowViewModel : ObservableObject
 {
-    
+    private readonly ILogger _logger;
+    private readonly IVariablesRepository _repository;
+
+    [ObservableProperty] private string? _errorMessage;
+
+    [ObservableProperty] private bool _isLoading;
+
+    public ObservableCollection<VariableViewModel> EnvironmentVariables { get; } = new();
+
+    public MainWindowViewModel(IVariablesRepository repository, ILogger logger)
+    {
+        _repository = repository;
+        _logger = logger;
+    }
+
+    [RelayCommand]
+    private async Task LoadVariablesAsync(CancellationToken token)
+    {
+        _isLoading = true;
+        _errorMessage = null;
+        _logger.Information("Запуск загрузки переменных среды в UI...");
+
+        try
+        {
+            EnvironmentVariables.Clear();
+
+            var variables = await _repository.GetAllVariablesAsync(token);
+
+            foreach (var variable in variables)
+            {
+                var itemViewModel = new VariableViewModel(variable, _repository, _logger);
+                EnvironmentVariables.Add(itemViewModel);
+            }
+
+            _logger.Information("Успешно загружено {Count} переменных в таблицу", EnvironmentVariables.Count);
+        }
+        catch (OperationCanceledException)
+        {
+            _logger.Warning("Операция загрузки переменных была отменена.");
+        }
+        catch (Exception ex)
+        {
+            _errorMessage = "Не удалось загрузить переменные среды.";
+            _logger.Error(ex, "Критическая ошибка при заполнении таблицы DataGrid данными");
+        }
+        finally
+        {
+            _isLoading = false;
+        }
+    }
 }
--- src/SmartMealService.GuiClient/ViewModels/VariableViewModel.cs
+++ src/SmartMealService.GuiClient/ViewModels/VariableViewModel.cs
@@ -1,15 +1,72 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 
+using Serilog;
+
+using SmartMealService.GuiClient.Application;
+using SmartMealService.GuiClient.Models;
+
 namespace SmartMealService.GuiClient.ViewModels;
 
 public partial class VariableViewModel : ObservableObject
 {
-    [ObservableProperty]
-    public partial string Key { get; set; }
+    private readonly CustomEnvironmentVariable _model;
+    private readonly IVariablesRepository _repository;
+    private readonly ILogger _logger;
+
+    public VariableViewModel(
+        CustomEnvironmentVariable model,
+        IVariablesRepository repository,
+        ILogger logger)
+    {
+        _model = model;
+        _repository = repository;
+        _logger = logger;
+
+        _key = model.Key;
+        _value = model.Value;
+        _comment = model.Comment ?? string.Empty;
+    }
 
     [ObservableProperty]
-    public partial string Value { get; set; }
+    private string _key;
 
     [ObservableProperty]
-    public partial string Comment { get; set; }
+    private string _value;
+
+    [ObservableProperty]
+    private string _comment;
+
+    partial void OnValueChanged(string value)
+    {
+        if (_model.Value == value) return;
+
+        _model.Value = value;
+
+        _logger.Information("Изменение переменной среды: {Key} = '{Value}'", _key, value);
+
+        Task.Run(async () => await SaveChangesAsync());
+    }
+
+    partial void OnCommentChanged(string? value)
+    {
+        if (_model.Comment == value) return;
+
+        _model.Comment = value;
+
+        _logger.Information("Изменение комментария для переменной {Key}: '{Comment}'", _key, value);
+
+        Task.Run(async () => await SaveChangesAsync());
+    }
+
+    private async Task SaveChangesAsync()
+    {
+        try
+        {
+            await _repository.UpdateVariableAsync(_model, CancellationToken.None);
+        }
+        catch (Exception ex)
+        {
+            _logger.Error(ex, "Ошибка при сохранении переменной {Key} в репозиторий", Key);
+        }
+    }
 }
