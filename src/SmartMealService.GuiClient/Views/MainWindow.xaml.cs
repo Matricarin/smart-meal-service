033ba7ce081d505bb7da18d7e2c754b94bf38295
matricarin <matricarin@gmail.com>
2026-06-13 13:18:15 +0500

    main window was updated

--- src/SmartMealService.GuiClient/Views/MainWindow.xaml.cs
+++ src/SmartMealService.GuiClient/Views/MainWindow.xaml.cs
@@ -1,12 +1,27 @@
 ﻿using System.Windows;
 
+using SmartMealService.GuiClient.ViewModels;
+
 namespace SmartMealService.GuiClient.Views
 {
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        private readonly MainWindowViewModel _viewModel;
+        public MainWindow(MainWindowViewModel viewModel)
         {
             InitializeComponent();
+
+            _viewModel = viewModel;
+            DataContext = _viewModel;
+
+            Loaded += MainWindow_Loaded;
+        }
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (_viewModel.LoadVariablesCommand.CanExecute(null))
+            {
+                await _viewModel.LoadVariablesCommand.ExecuteAsync(null);
+            }
         }
     }
 }
