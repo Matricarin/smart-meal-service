5a4813c1560c1f6658d2be66452e3091740061e8
matricarin <matricarin@gmail.com>
2026-06-13 12:26:22 +0500

    view models and views was added

---
+++ src/SmartMealService.GuiClient/Models/CustomEnvironmentVariable.cs
@@ -0,0 +1,8 @@
+﻿namespace SmartMealService.GuiClient.Models;
+
+public sealed class CustomEnvironmentVariable
+{
+    public required string Key { get; set; } = string.Empty;
+    public required string Value { get; set; } = string.Empty;
+    public string? Comment { get; set; }
+}
---
+++ src/SmartMealService.GuiClient/Views/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace SmartMealService.GuiClient.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
